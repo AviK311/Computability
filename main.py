30cbf6a042aed3b3a4ea75df64f29bf02cfeabfc
AviK311 <45146057+AviK311@users.noreply.github.com>
2020-06-28 09:37:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Parse_Program import encode_file as encode_f, encode_str as encode
-from decode_to_program import decode
+from Parse_Program import encode_file as encode_f, encode_str as encode, encode_instruction
+from decode_to_program import decode, decode_instruction
 from coding import pair_encode, pair_decode, list_encode, list_decode, get_a_b_c, encode_a_b_c
 from Extended import *
 
